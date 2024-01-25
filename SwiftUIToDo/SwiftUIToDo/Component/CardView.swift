e981d1c86b9cefbc9b1816869caed5ae904a26e1
abdur999 <abdursrahims@icloud.com>
2024-01-26 02:07:20 +0530

    craete complex layout using custom view, make card shape object, use symbol, use image and color from asset. Files are CardView.swift, CustomButtonView.swift, CustomBackgroundView.swift, HikeView.swift

--- SwiftUIToDo/SwiftUIToDo/Component/CardView.swift
+++ SwiftUIToDo/SwiftUIToDo/Component/CardView.swift
@@ -9,23 +9,51 @@ import SwiftUI
 
 struct CardView: View {
     var body: some View {
+        
         ZStack {
-            Circle()
-                .fill(
-                    LinearGradient(
-                        colors: [
-                            Color("ColorIndigoMedium"),
-                            Color("ColorsSalmonLight")
-                        ],
-                        startPoint: .topLeading,
-                        endPoint: .bottomTrailing
-                    )
-                )
-                .frame(width: 256,height: 256)
-            Image("image-1")
-                .resizable()
-                .scaledToFit()
+            CustomBackgroundView()
+             VStack {
+                 VStack(alignment: .leading) {
+                     HStack {
+                         Text("Hiking")
+                             .fontWeight(.black)
+                             .font(.system(size: 50))
+                             .foregroundStyle(LinearGradient(colors: [Color.customGrayLight, Color.customGrayMedium], startPoint: .top, endPoint: .bottom))
+                         
+                         Button {
+                             //ACTION: SHOW a sheet
+                             print("The button pressed")
+                         } label: {
+                            CustomButtonView()
+                         }
+                     }
+                     
+                     Text("Fun and enjoyable activities for friends and families")
+                         .multilineTextAlignment(.leading)
+                         .italic()
+                         .foregroundColor(.customGrayMedium)
+                 }
+                 
+                 ZStack {
+                    Circle()
+                        .fill(
+                            LinearGradient(
+                                colors: [
+                                    Color("ColorIndigoMedium"),
+                                    Color("ColorsSalmonLight")
+                                ],
+                                startPoint: .topLeading,
+                                endPoint: .bottomTrailing
+                            )
+                        )
+                        .frame(width: 200,height: 200)
+                    Image("image-1")
+                        .resizable()
+                        .scaledToFit()
+                }
+            }
         }
+        .frame(width: 300, height: 400)
     }
 }
 
--- SwiftUIToDo/SwiftUIToDo/ContentView.swift
+++ SwiftUIToDo/SwiftUIToDo/ContentView.swift
@@ -56,6 +56,7 @@ struct ContentView: View {
 }
 
 #Preview {
-    ContentView()
-        .modelContainer(for: Item.self, inMemory: true)
+//    ContentView()
+//        .modelContainer(for: Item.self, inMemory: true)
+    HikeView()
 }
