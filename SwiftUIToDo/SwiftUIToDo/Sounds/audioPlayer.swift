a510a31028851d72470e4d6ad33a224fd785f2a4
abdur999 <abdursrahims@icloud.com>
2024-01-31 00:19:09 +0530

    app complex uidesign with swift and various type of animation

---
+++ SwiftUIToDo/SwiftUIToDo/Sounds/audioPlayer.swift
@@ -0,0 +1,18 @@
+//
+//  audioPlayer.swift
+//  SwiftUIToDo
+//
+//  Created by Abdur Rahim on 30/01/24.
+//
+
+import SwiftUI
+
+struct audioPlayer: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    audioPlayer()
+}
---
+++ SwiftUIToDo/SwiftUIToDo/Views/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  SwiftUIToDo
+//
+//  Created by Abdur Rahim on 28/01/24.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
